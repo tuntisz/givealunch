5a5ace2f8c9961e880e0530be85a4b4048d4f1fd
Tyler <tuntisz@gmail.com>
2020-03-31 22:08:05 -0600

    Updates

---
+++ component---src-pages-404-tsx-25e2027300ce0d07efdb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{208:function(e,n,t){"use strict";t.r(n);var a=t(1),l=t(68),r=t.n(l),c=t(67),o=t(201),u=t.n(o),i=function(e){var n=e.children,t=e.container?a.createElement(c.a,null,n):n;return a.createElement("section",{className:u.a.segment},t)};i.defaultProps={container:!0};n.default=function(){return a.createElement(a.Fragment,null,a.createElement(r.a,{title:"404 Not Found"}),a.createElement(i,null,a.createElement("h1",null,"Page was not found")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-25e2027300ce0d07efdb.js.map
